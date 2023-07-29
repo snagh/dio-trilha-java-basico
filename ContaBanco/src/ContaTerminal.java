5ebae7da1de8e3075f42a90797c763921cf09f3a
snagh <andrews.cs16@gmail.com>
2023-07-29 16:06:24 -0300

    Ajustes

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar a classe Scanner
+        //Conhecer e importar a classe Scanner
 
         //Exibir as mensagens para o nosso usuário
         
