6ef64e96e2706cac4bb308bca50ceaf68930822c
snagh <andrews.cs16@gmail.com>
2023-07-29 16:03:30 -0300

    Desafio completo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,8 @@
+
+import java.util.Scanner;
+import java.util.Locale;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO: Conhecer e importar a classe Scanner
@@ -7,5 +12,24 @@ public class ContaTerminal {
         //Obter pela Scanner os valores digitados no terminal
 
         //Exibir a mensagem 'conta criada'
+
+        //criando o objeto scanner
+
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, informe seu nome: ");
+            String nome = scanner.nextLine();
+            System.out.println("Agora informe o número da conta com dígito: ");
+            String numeroConta = scanner.nextLine();
+            System.out.println("Qual a agência? ");
+            String agencia = scanner.nextLine();
+            System.out.println( "Agora informe o saldo: ");
+            float saldo = scanner.nextFloat();
+
+
+            System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nossso banco, sua agência é " + agencia + "conta " + numeroConta + ", e seu saldo de " + saldo + "já está disponível para saque.");
+        }
+        
+
+
     }
 }
