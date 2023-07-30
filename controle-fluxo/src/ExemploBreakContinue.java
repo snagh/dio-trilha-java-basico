10d8ed8a9a7e052fd8b7e2dede84c303cc9b895b
snagh <andrews.cs16@gmail.com>
2023-07-30 15:47:45 -0300

    Exemplo de break e continue e while

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,17 @@
+// class ExemploBreakContinue.java
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+		if(numero==3)
+			break;
+		
+		System.out.println(numero);
+		
+	}
+    //O breake interrompe o bloco de código caso seja satisfeita a condição proposta
+    //O conitnue não interrompe a execução do laço mas ignora as instruções que vêm após o continue
+	//Qual a saída no console ?
+
+    }
+}
