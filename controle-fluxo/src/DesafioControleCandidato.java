9660a6b3beaa24ddd5147e1e901df15dfec443e6
snagh <andrews.cs16@gmail.com>
2023-08-03 11:11:50 -0300

    Desafio completo.

--- controle-fluxo/src/DesafioControleCandidato.java
+++ controle-fluxo/src/DesafioControleCandidato.java
@@ -1,9 +1,55 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class DesafioControleCandidato {
     public static void main(String[] args) {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);            
+        }
+               
+    }
 
-        selecaoCandidatos();
-        
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizadas++;
+            }
+            else{
+                System.out.println("CONTATO REALIZADO COM SUCESSO!");
+            }
+
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADAS" );
+
+
+    }
+
+    //método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
+
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento:/n");
+        for(int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " se chama " + candidatos[indice]);
+        }
+        System.out.println("Forma abreviada do For que não mostra o índice");
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
@@ -15,7 +61,7 @@ public class DesafioControleCandidato {
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5) {
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos [candidatoAtual];
             double salarioPretendido = valorPretendido();
             System.out.println("O candidato " + candidato + " solicitou esse valor de salário: " + salarioPretendido);
