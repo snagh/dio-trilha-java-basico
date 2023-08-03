34f512affe11be0ab3787196fbc7de75bc7f5de8
snagh <andrews.cs16@gmail.com>
2023-08-03 10:37:09 -0300

    Desafio quase completo

--- controle-fluxo/src/DesafioControleCandidato.java
+++ controle-fluxo/src/DesafioControleCandidato.java
@@ -1,10 +1,37 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class DesafioControleCandidato {
     public static void main(String[] args) {
+
+        selecaoCandidatos();
         
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
     }
+
+    static void selecaoCandidatos() {
+
+        // Array com a lista de candidatos
+
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos [candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato " + candidato + " solicitou esse valor de salário: " + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga!");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }    
+
+    }
+
+    static double valorPretendido() {
+     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
 
     static void analisarCandidato (double salarioPretendido) {
         double salarioBase = 2000.00;
